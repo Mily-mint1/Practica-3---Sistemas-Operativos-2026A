3d211199f2202d28cc0ec3df2bac71317079f791
Daniel Tixicuro <danieltixi13@gmail.com>
2026-05-13 04:36:11 +0000

    Version Final Ejercicio 03

--- Ejercicio03_Figura03_Modificado/programa03.c
+++ Ejercicio03_Figura03_Modificado/programa03.c
@@ -6,20 +6,30 @@
 int pid;
 
 int main () {
-    pid = fork();
+    // Creamos una copia del programa original para iniciar el proceso hijo
+    pid = fork(); 
+    
     switch (pid)
     {
         case -1:
+            // El sistema no pudo crear el nuevo proceso por algun error
             printf ("\nNo he podido crear el proceso hijo");
             break;
+            
         case 0:
+            // Aqui inicia el codigo que solo va a ejecutar el hijo
             printf ("\nSoy el hijo, mi PID es %d y mi PPID es %d", getpid(), getppid());
             break;
+            
         default:
-            wait(NULL);
+            // El padre se detiene a esperar que el hijo termine su tarea
+            wait(NULL); 
+            // Aqui inicia el codigo que solo va a ejecutar el padre
             printf ("\nSoy el padre, mi PID es %d y el PID de mi hijo es %d", getpid(), pid);
             break;
     }
+    
+    // Este mensaje final lo imprimen ambos procesos antes de cerrarse
     printf ("\nFinal de ejecución de %d \n", getpid());
     exit (0);
 }
